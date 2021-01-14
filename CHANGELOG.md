ecbd07638dd39c02925089cd028e14bd7eacd71e
Jon Carstens <jjcarstens@me.com>
2021-01-14 08:31:57 -0700

    v0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Changelog
 
+## 0.1.1
+
+* Bug Fixes
+  * Fix `Logger.debug/1` undefined error
+  * Adjusted polling time for more accurate measurements
+  * Fixed `ethanol` misspelling
+
+* Enhancements
+  * Remove then "measuring..." log message to keep logs clean
+
 ## 0.1.0
 
 * Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,8 @@
 defmodule Sgp30.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
+  @source_url "https://github.com/jjcarstens/sgp30"
 
   def project do
     [
@@ -12,18 +13,21 @@ defmodule Sgp30.MixProject do
       deps: deps(),
       description: "Interface with SGP30 gas sensor in Elixir",
       docs: docs(),
-      package: package()
+      package: package(),
+      preferred_cli_env: [
+        docs: :docs,
+        "hex.build": :docs,
+        "hex.publish": :docs
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:circuits_i2c, "~> 0.3"},
@@ -33,16 +37,28 @@ defmodule Sgp30.MixProject do
 
   defp docs do
     [
-      extras: ["README.md"],
+      extras: ["README.md", "CHANGELOG.md"],
       main: "readme",
       source_ref: "v#{@version}",
-      source_url: "https://github.com/jjcarstens/sgp30"
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 
   defp package do
     [
-      links: %{"Github" => "https://github.com/jjcarstens/sgp30"},
+      files: [
+        "CHANGELOG.md",
+        "lib",
+        "LICENSE",
+        "mix.exs",
+        "README.md"
+      ],
+      links: %{
+        "Github" => @source_url,
+        "Datasheet" =>
+          "https://www.mouser.com/datasheet/2/682/Sensirion_Gas_Sensors_SGP30_Datasheet_EN-1148053.pdf"
+      },
       licenses: ["Apache-2.0"]
     ]
   end
