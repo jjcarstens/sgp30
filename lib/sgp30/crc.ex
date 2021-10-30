554904debfb2702e38a51d674de76d1ca7990483
Scott <dscottwiggins@gmail.com>
2021-10-30 13:11:49 -0400

    switch to erlang crc library

--- lib/sgp30/crc.ex
+++ lib/sgp30/crc.ex
@@ -1,4 +1,6 @@
 defmodule SGP30.CRC do
+  @crc_alg :cerlc.init({8, 0x31, 0xFF, 0x00, false})
+
   def check(data_crc_tuples) when is_list(data_crc_tuples) do
     data_crc_tuples
     |> Enum.all?(&check/1)
@@ -10,13 +12,6 @@ defmodule SGP30.CRC do
 
   def calculate(data) do
     <<data::16>>
-    |> CRC.calculate(%{
-      width: 8,
-      poly: 0x31,
-      init: 0xFF,
-      refin: false,
-      refout: false,
-      xorout: 0x00
-    })
+    |> :cerlc.calc_crc(@crc_alg)
   end
 end
